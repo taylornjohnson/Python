469f2f8b436bd5e3ac5b1cbbaac3b5c6c0b8d671
taylornjohnson <johnson.taylornicole@gmail.com>
2021-06-04 16:08:29 -0400

    corrected pypoll

--- PyPoll/pypoll_analysis.py
+++ PyPoll/pypoll_analysis.py
@@ -6,61 +6,57 @@ outpath = os.path.join("Analysis", "pybank_analysis.txt")
 #Set variables
 total_votes = 0
 all_candidates = []
-vote_count  = []
+candidate_votes = {}
+
+winning_candidate = ""
+win_count = 0
+
 #Open file
 with open(csvpath) as pollfile:
- csvreader = csv.reader(pollfile)
- for row in csvreader:
-  line = next(csvreader,None)
+  csvreader = csv.DictReader(pollfile)
+ 
+for row in csvreader:
 #Total number of votes cast
-  total_votes += 1
-  candidate = row[2]
-        #if candidate has other votes then add to vote tally
-  if candidate in all_candidates:
-    candidate = all_candidates.index(candidate)
-    vote_count[all_candidates] += 1
-        #else create new spot in list for candidate
-  else:
+  total_votes = total_votes + 1
+
+  candidate = row["Candidate"]
+
+  #if candidate has other votes then add to vote tally
+  if candidate not in all_candidates:
     all_candidates.append(candidate)
-    vote_count.append(1)
-#Variables calculated outside the loop
-percentages = []
-max_votes = vote_count[0]
-max_index = 0
-#Percentage of votes per candidate and Winner
-for count in range(len(all_candidates)):
-    vote_percentage = vote_count[count]/total_votes*100
-    percentages.append(vote_percentage)
-    if vote_count[count] > max_votes:
-        max_votes = vote_count[count]
-        print(max_votes)
-        max_index = count
-winner = all_candidates[max_index]
+    candidate_votes[candidate] = 0
+    candidate_votes[candidate] += 1
+  
+with open(outpath, "w") as txt_file:
+  
+  #print results
+  election_results = ()
+  print("Election Results")
+  print("--------------------------")
+  print(f"Total Votes: {total_votes}")
+)
+print(election_results)
 
-#print results
-print("Election Results")
-print("--------------------------")
-print(f"Total Votes: {total_votes}")
-for count in range(len(all_candidates)):
-    print(f"{all_candidates[count]}: {percentages[count]}% ({vote_count[count]})")
-print("---------------------------")
-print(f"Winner: {winner}")
-print("---------------------------")
+txt_file.write(election_results)
 
-write_file = f"pypoll_results_summary.txt"
+# find winner
+for candidate in candidate_votes:
+  votes = candidate_votes.get(candidate)
+  vote_percent = float(votes)/float(total_votes) * 100
+  
+  if (votes > win_count):
+    win_count = votes
+    winning_candidate = candidate
 
-#open write file
-filewriter = open(write_file, mode = 'w')
+  voter_output = f"{candidate}: {vote_percent:.2f}% ({votes})\n"
+  print(voter_output)
 
-#print analysis to file
-filewriter.write("Election Results\n")
-filewriter.write("--------------------------\n")
-filewriter.write(f"Total Votes: {total_votes}\n")
-for count in range(len(all_candidates)):
-    filewriter.write(f"{all_candidates[count]}: {percentages[count]}% ({vote_count[count]})\n")
-filewriter.write("---------------------------\n")
-filewriter.write(f"Winner: {winner}\n")
-filewriter.write("---------------------------\n")
+#winner
+winner_sum = (
+  f"--------------------------\n"
+  f"Winner: {winning_candidate}\n"
+  f"--------------------------\n"
+)
+print(winner_sum)
 
-#close file
-filewriter.close()
+txt_file.write(winner_sum)
